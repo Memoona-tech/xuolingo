d8a71660eca871d277107716fe5944ddc4e1087f
Memoona-tech <memoonas2024@gmail.com>
2025-03-28 14:44:03 +0500

    lesson id file

--- app/lesson/[lessonId]/page.tsx
+++ app/lesson/[lessonId]/page.tsx
@@ -2,48 +2,45 @@ import { getLesson, getUserProgress, getUserSubscription } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Quiz } from "../quiz";
 
-interface Props{
-    params:{
-        lessonId: number;
-    }
+interface Props {
+  params: {
+    lessonId: string; // Changed from number to string
+  }
 }
 
-const LessonIdPage = async({
-    params,
-}: Props) =>{
-    const lessonData = getLesson(params.lessonId);
-    const userProgressData = getUserProgress();
-    const userSubscriptionData = getUserSubscription();
+const LessonIdPage = async ({ params }: Props) => {
+  // Convert lessonId to number for your queries if needed.
+  const lessonData = getLesson(Number(params.lessonId));
+  const userProgressData = getUserProgress();
+  const userSubscriptionData = getUserSubscription();
 
-    const [
-        lesson,
-        userProgress,
-        userSubscription,
-    ] = await Promise.all([
-        lessonData,
-        userProgressData,
-        userSubscriptionData,
-    ]);
+  const [lesson, userProgress, userSubscription] = await Promise.all([
+    lessonData,
+    userProgressData,
+    userSubscriptionData,
+  ]);
 
-if(!lesson || !userProgress){
+  if (!lesson || !userProgress) {
     redirect("/learn");
-}
+  }
 
-const initialPercentage = lesson.challenges
-.filter((challenge)=> challenge.completed)
-.length / lesson.challenges.length * 100;
-    return (
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
 
-        
-        <div className="min-h-screen flex flex-col">
-            <Quiz
-            initialLessonId={lesson.id}
-            initialLessonChallanges={lesson.challenges}
-            initialHearts={userProgress.hearts}
-            initialPoints={userProgress.points}
-            initialPercentage={initialPercentage}
-            userSubscription={userSubscription} />
-        </div>
-    )
-}
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Quiz
+        initialLessonId={lesson.id}
+        initialLessonChallanges={lesson.challenges}
+        initialHearts={userProgress.hearts}
+        initialPoints={userProgress.points}
+        initialPercentage={initialPercentage}
+        userSubscription={userSubscription}
+      />
+    </div>
+  );
+};
+
 export default LessonIdPage;
